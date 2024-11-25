46ad25100ae8aba590d220997eb375357f01fd66
ohto keskilammi <voyager-2021@outlook.com>
2024-11-25 18:28:45 +0200

    Initial commit

---
+++ src/be6502emu/core.py
@@ -0,0 +1,26 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+dpg.show_documentation()
+dpg.show_style_editor()
+dpg.show_debug()
+dpg.show_about()
+dpg.show_metrics()
+dpg.show_font_manager()
+dpg.show_item_registry()
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Save")
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.create_viewport(title='be6502emu', width=800, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+
+while dpg.is_dearpygui_running():
+    dpg.render_dearpygui_frame()
+
+dpg.destroy_context()
